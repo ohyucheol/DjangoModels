1de17d8df3c151e350eb114c07a70e372be9560c
ohyucheol <oufe@pusan.ac.kr>
2022-07-30 23:32:40 +0900

    working a00 templates

---
+++ a00/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'DjangoApps/templates/a00/signup.html')
+
+def signup(request):
+    return render(request, 'DjangoApps/templates/a00/signup.html')
+
+def signin(request):
+    return render(request, 'DjangoApps/templates/a00/signup.html')
