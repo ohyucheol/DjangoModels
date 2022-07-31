04aff911d46c4bf0e1482f49fa997343e0a4abe3
ohyucheol <oufe@pusan.ac.kr>
2022-07-31 23:40:49 +0900

    under construction

--- a00/views.py
+++ a00/views.py
@@ -12,14 +12,17 @@ def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
 
+        #checking all items are filled
         if form.is_valid():
             data = form.cleaned_data
 
+            #checking password is correct
             if data['password'] != data['password2']:
                 form = SignupForm()
                 context = {'message':'비밀번호가 일치하지 않습니다', 'form':form}
                 return render(request, 'DjangoApps/templates/a00/signup.html', context)
 
+            #checking email is correct
             try:
                 validate_email(data['username'])
             except: #ValidationError
@@ -27,17 +30,26 @@ def signup(request):
                 context = {'message':'이메일 형식이 바르지 않습니다', 'form':form}
                 return render(request, 'DjangoApps/templates/a00/signup.html', context)
 
-            if data['password'] == data['password2']:
+            #checking user is already exist
+            try:
                 user = User.objects.create_user(username=data['username'], password=data['password'])
-                user = authenticate(request, username=data['username'], password=data['password'])
-                login(request, user)
-                return redirect('a00:index')
-
-            else:
+            except:
                 form = SignupForm()
-                context = {'message':'이미 가입한 아이디입니다', 'form':form}
+                context = {'message':'이미 가입한 이메일입니다', 'form':form}
                 return render(request, 'DjangoApps/templates/a00/signup.html', context)
 
+            #pass all conditions
+            user = authenticate(request, username=data['username'], password=data['password'])
+            login(request, user)
+            return redirect('a00:index')
+
+        #when some items are not filled
+        else:
+            form = SignupForm()
+            context = {'message':'이메일과 비밀번호를 입력해주세요', 'form':form}
+            return render(request, 'DjangoApps/templates/a00/signup.html', context)
+
+    #when user visits signup page
     else:
         form = SignupForm()
         return render(request, 'DjangoApps/templates/a00/signup.html', {'form':form})
@@ -46,6 +58,7 @@ def signin(request):
     if request.method == 'POST':
         form = SigninForm(request.POST)
 
+        #checking all items are filled
         if form.is_valid():
             data = form.cleaned_data
 
@@ -72,7 +85,10 @@ def signin(request):
                 form = SigninForm()
                 context = {'message':'비밀번호를 확인해주세요', 'form':form}
                 return render(request, 'DjangoApps/templates/a00/signin.html', context)
-
+        else:
+            form = SigninForm()
+            context = {'message':'이메일과 비밀번호를 입력해주세요', 'form':form}
+            return render(request, 'DjangoApps/templates/a00/signin.html', context)
     else:
         form = SigninForm()
         return render(request, 'DjangoApps/templates/a00/signin.html', {'form':form})
