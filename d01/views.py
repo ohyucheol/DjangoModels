2b9d215af848ed3f85f27af5464f4e1c7498b0be
ohyucheol <oufe@pusan.ac.kr>
2022-08-07 20:14:02 +0900

    README.md, D01 : 과업의 목적 수정

--- d01/views.py
+++ d01/views.py
@@ -3,16 +3,14 @@ from .models import Book, BookMeta, Format, Category
 # Create your views here.
 
 def index(request):
-    # books = Book.objects.all()
-    # formats = Format.objects.all()
-
-    # bookq = Book.objects.select_related('book')
     books = Book.objects.all().values()
 
+    #Book의 id를 기준으로 Format, Category 등을 받는다
     for b in books:
         formats = Format.objects.filter(book=b['id'])
-        # for f in formats:
-        #     b['format'] = f
-        b['format'] = formats
+        b['formats'] = formats
+
+        categories = Category.objects.filter(book=b['id'])
+        b['categories'] = categories
 
     return render(request, 'DjangoApps/templates/D01/index.html', {'books' : books})
