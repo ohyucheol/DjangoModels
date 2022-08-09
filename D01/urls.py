bbb5b5a87445f3e9162885384b569270011d48a3
ohyucheol <oufe@pusan.ac.kr>
2022-08-09 20:43:59 +0900

    D01:url 정비에 따른 views, templates 작성

--- D01/urls.py
+++ D01/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
-from .views import BookListView
+from .views import About, BookListView
 
 app_name = 'D01'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('list/', BookListView.as_view(), name='BookListView')
+    path('', About.as_view(), name='about'),
+    path('book/', BookListView.as_view(), name='booklist')
     # path('create', CreateView.as_view())
     # path('signup/', views.signup, name='signup'),
     # path('signin/', views.signin, name='signin'),
--- D01/views.py
+++ D01/views.py
@@ -1,39 +1,44 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 from django.views.generic.list import ListView
 from .models import Book, Format, DecimalClassification
 # Create your views here.
 
-def index(request):
-    books = Book.objects.all().values()
+# def index(request):
+#     books = Book.objects.all().values()
 
-    #Book의 id를 기준으로 Format, DecimalClassification 등을 받는다
-    for b in books:
-        formats = Format.objects.filter(book_id=b['id'])
-        b['formats'] = formats
+#     #Book의 id를 기준으로 Format, DecimalClassification 등을 받는다
+#     for b in books:
+#         formats = Format.objects.filter(book_id=b['id'])
+#         b['formats'] = formats
 
-        # categories = DecimalClassification.objects.filter(book=b['id'])
-        # b['categories'] = categories
+#         # categories = DecimalClassification.objects.filter(book=b['id'])
+#         # b['categories'] = categories
 
-    return render(request, 'DjangoApps/templates/D01/index.html', {'books' : books})
+#     return render(request, 'DjangoApps/templates/D01/index.html', {'books' : books})
 
+class About(TemplateView):
+    template_name = "DjangoApps/templates/D01/about.html"
 
 class BookListView(ListView):
 
     model = Book
-    paginate_by = 1
+    paginate_by = 12
     context_object_name = 'books'
-    template_name = 'DjangoApps/templates/D01/booklistview.html'
+    template_name = 'DjangoApps/templates/D01/booklist.html'
 
-    def get_queryset(self):
-        queryset = super(BookListView, self).get_queryset().values()
+    # queryset 내의 추가적인 context_data를 위한 오버라이드
+    # def get_queryset(self):
+    #     queryset = super(BookListView, self).get_queryset().values()
 
-        for book in queryset:
-            formats = Format.objects.filter(book_id=book['id'])
-            book['formats'] = formats
+    #     for book in queryset:
+    #         formats = Format.objects.filter(book_id=book['id'])
+    #         book['formats'] = formats
 
-        return queryset
+    #     return queryset
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['aa'] = 'aa'
-        return context
+    # 추가적인 context data를 위한 오버라이드
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['aa'] = 'aa'
+    #     return context
