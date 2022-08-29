0df2972257bb536ea8647ae624a26a7359230f3e
ohyucheol <oufe@pusan.ac.kr>
2022-08-29 22:56:50 +0900

    J01:다수의 파일을 업로드하고 그 처리시간동안 진행중임을 표시하는 스피너 작성

--- J01/views.py
+++ J01/views.py
@@ -16,26 +16,40 @@ def about(request):
 def list(request):
     s3 = boto3.resource('s3')
     bucket = s3.Bucket('testbucket.djangoapps')
-    objects = bucket.objects.all()
 
     if request.method == 'POST':
         keys = request.POST.getlist('keys[]')
         for k in keys:
             response = bucket.delete_objects(Delete={'Objects':[ {'Key': k} ]})
-            
+
         objects = bucket.objects.all()
         return render(request, 'DjangoApps/templates/J01/list.html', {'objects':objects})
     else:
+        objects = bucket.objects.all()
         return render(request, 'DjangoApps/templates/J01/list.html', {'objects':objects})
 
+# def upload(request):
+#     s3 = boto3.resource('s3')
+#     bucket = s3.Bucket('testbucket.djangoapps')
+    
+#     if request.method == 'POST':
+#         bucket.put_object(Body=request.FILES['file'], Key=request.FILES['file'].name)
+
+#         return redirect('/J01/list')
+#         # return render(request, 'DjangoApps/templates/J01/upload.html', {'request':request})
+
+#     else:
+#         return render(request, 'DjangoApps/templates/J01/upload.html')
+
 def upload(request):
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket('testbucket.djangoapps')
+    
     if request.method == 'POST':
-        s3 = boto3.resource('s3')
-        bucket = s3.Bucket('testbucket.djangoapps')
+        files = request.FILES.getlist('files')
+        for f in files:
+            bucket.put_object(Body=f, Key=f.name)
 
-        bucket.put_object(Body=request.FILES['file'], Key=request.FILES['file'].name)
-
-        # pass
         return redirect('/J01/list')
         # return render(request, 'DjangoApps/templates/J01/upload.html', {'request':request})
 
