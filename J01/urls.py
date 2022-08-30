b17ce1627ef91b687f3da80af02152507cd66a50
ohyucheol <oufe@pusan.ac.kr>
2022-08-30 14:00:06 +0900

    J01:tinymce에서 파일을 관리하고 그 파일의 링크를 복사하여 첨부할 수 있도록 함

--- J01/urls.py
+++ J01/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.about, name='about'),
     path('list', views.list, name='list'),
     path('upload', views.upload, name='upload'),
+    path('modal-list', views.modal_list, name='modal-list'),
+    path('modal-upload', views.modal_upload, name='modal-upload'),
 ]
--- J01/views.py
+++ J01/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 import boto3
+from django.views.decorators.clickjacking import xframe_options_exempt
 # Create your views here.
 
 def about(request):
@@ -28,19 +29,6 @@ def list(request):
         objects = bucket.objects.all()
         return render(request, 'DjangoApps/templates/J01/list.html', {'objects':objects})
 
-# def upload(request):
-#     s3 = boto3.resource('s3')
-#     bucket = s3.Bucket('testbucket.djangoapps')
-    
-#     if request.method == 'POST':
-#         bucket.put_object(Body=request.FILES['file'], Key=request.FILES['file'].name)
-
-#         return redirect('/J01/list')
-#         # return render(request, 'DjangoApps/templates/J01/upload.html', {'request':request})
-
-#     else:
-#         return render(request, 'DjangoApps/templates/J01/upload.html')
-
 def upload(request):
     s3 = boto3.resource('s3')
     bucket = s3.Bucket('testbucket.djangoapps')
@@ -51,7 +39,37 @@ def upload(request):
             bucket.put_object(Body=f, Key=f.name)
 
         return redirect('/J01/list')
-        # return render(request, 'DjangoApps/templates/J01/upload.html', {'request':request})
 
     else:
         return render(request, 'DjangoApps/templates/J01/upload.html')
+
+@xframe_options_exempt
+def modal_list(request):
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket('testbucket.djangoapps')
+
+    if request.method == 'POST':
+        keys = request.POST.getlist('keys[]')
+        for k in keys:
+            response = bucket.delete_objects(Delete={'Objects':[ {'Key': k} ]})
+
+        objects = bucket.objects.all()
+        return render(request, 'DjangoApps/templates/J01/modal-list.html', {'objects':objects})
+    else:
+        objects = bucket.objects.all()
+        return render(request, 'DjangoApps/templates/J01/modal-list.html', {'objects':objects})
+
+@xframe_options_exempt
+def modal_upload(request):
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket('testbucket.djangoapps')
+    
+    if request.method == 'POST':
+        files = request.FILES.getlist('files')
+        for f in files:
+            bucket.put_object(Body=f, Key=f.name)
+
+        return redirect('/J01/modal-list')
+
+    else:
+        return render(request, 'DjangoApps/templates/J01/modal-upload.html')
