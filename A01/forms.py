aa1d8d99742832eb8ca05c73fcab7640d45a8f50
ohyucheol <oufe@pusan.ac.kr>
2022-09-05 15:10:12 +0900

    A01: validator, validation error, widget을 별도의 FormAttributes의 명칭을 FormDecorator로 변경함

--- A01/forms.py
+++ A01/forms.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth.validators import ASCIIUsernameValidator
 from django.core.validators import RegexValidator, EmailValidator
 
-# validator, validation error, widget 등의 구성을 위한 FormAttributes
-class FormAttributes:
+# validator, validation error, widget 등의 구성을 위한 FormDecorator
+class FormDecorator:
     # username을 검증하는 RegexValidator에는 username이 unique인지 판단하지 않는다.
     # 그럼에도 불구하고 unique error가 발생하는 것은 User 모델의 username field에서 unique 여부를 판단하기 때문이다.
     # 즉, ModelForm의 field는 form의 validation, model의 validation에서 각각 검증된다.
@@ -23,17 +23,17 @@ class FormAttributes:
 
 # validator 등을 부가한 field를 포함하는 ModelForm의 구성을 위한 UserCreateForm
 class UserCreateForm(forms.ModelForm):
-    a = FormAttributes()
+    d = FormDecorator()
 
     # 모델에 없는 field 또는 별도의 기능(validation 등)이 필요한 field들은 다음과 같이 새로 정의할 수 있다.
     # 새로 정의한 field의 경우 기본적으로 required 속성을 가지며 입력 여부가 validation의 대상이 된다.
     # 예컨대 email의 경우 입력하지 않는다면 required 속성에 의하여 한 번, EmailValidator()에 의하여 한 번,
     # 총 두 번의 required error가 발생한다.
 
-    username = forms.CharField(widget=a.wid_text, error_messages=a.err_username, validators=[a.val_azAZ09])
-    email = forms.EmailField(widget=a.wid_email, error_messages=a.err_email, validators=[a.val_email])
-    password1 = forms.CharField(widget=a.wid_password, error_messages=a.err_password)
-    password2 = forms.CharField(widget=a.wid_password, error_messages=a.err_password)
+    username = forms.CharField(widget=d.wid_text, error_messages=d.err_username, validators=[d.val_azAZ09])
+    email = forms.EmailField(widget=d.wid_email, error_messages=d.err_email, validators=[d.val_email])
+    password1 = forms.CharField(widget=d.wid_password, error_messages=d.err_password)
+    password2 = forms.CharField(widget=d.wid_password, error_messages=d.err_password)
 
     class Meta:
         # 이 ModelForm과 관련한 Model과 그 field를 지정하는 부분으로써 여기에서 지정한 field에 대해서는
