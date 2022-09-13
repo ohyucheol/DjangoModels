eb0bba7f561ab5061f15e068bd59442e99bc548b
ohyucheol <oufe@pusan.ac.kr>
2022-09-13 10:55:34 +0900

    F01: tag를 이용한 검색기능 작성

--- F01/views.py
+++ F01/views.py
@@ -8,6 +8,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import UpdateView, CreateView, DeleteView
 from django.views.decorators.clickjacking import xframe_options_sameorigin
+from django.contrib.auth.models import User
 from .models import Article
 from .forms import ArticleModelForm
 
@@ -23,10 +24,40 @@ class ListArticleView(ListView):
     context_object_name = 'Article'
     template_name = 'DjangoApps/templates/F01/list-article.html'
 
+    def get_queryset(self):
+
+        # 기본 queryset을 가져온다.
+        queryset = super().get_queryset()
+
+        # user_id로 작성자의 username을 찾아 user_id를 username으로 변경한다.
+        for a in queryset:
+            user = User.objects.filter(id=a.user_id)
+            # user queryset의 첫번째 원소의 username을 취한다.
+            a.user_id = user[0].username
+
+        # tag가 설정되어 있는 경우 해당 tag를 포함하는 Article queryset을 가져온다.
+        if 'tag' in self.request.GET:
+            queryset = Article.objects.all().filter(tag__contains=self.request.GET['tag'])
+
+        # 가져온 queryset의 모든 Article object에 대하여 공백으로 구분된 tag 값을 list로 변경한다.
+        for q in queryset:
+            q.tag = q.tag.split()
+        return queryset
+
 class DetailArticleView(DetailView):
     model = Article
     context_object_name = 'Article'
     template_name = 'DjangoApps/templates/F01/detail-article.html'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object()
+
+        # user_id로 작성자의 username을 찾아 user_id를 username으로 변경한다.
+        user = User.objects.filter(id=obj.user_id)
+        # user queryset의 첫번째 원소의 username을 취한다.
+        obj.user_id = user[0].username
+
+        return obj
 
 class CreateArticleView(CreateView):
     model = Article
@@ -82,6 +113,9 @@ class UpdateArticleView(UpdateView):
 
     def form_valid(self, form):
         data = form.cleaned_data
+
+        self.object.user_id = self.request.user.id
+        self.object.modified = datetime.datetime.now()
 
         if data['thumbnail_file'] != None:
             s3 = boto3.resource('s3')
