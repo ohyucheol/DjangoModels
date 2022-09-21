49125fab4a0fc99dbbde68beef28a432ed1904af
ohyucheol <oufe@pusan.ac.kr>
2022-09-21 20:42:21 +0900

    미분류 앱 삭제(u00,a01,b01,J01 등

--- J01/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class J01Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'DjangoApps.J01'
--- u01/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class U01Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'DjangoApps.u01'
